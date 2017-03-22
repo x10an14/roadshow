abdfffc5e36170b421e36f5562008446b13c0f3a
Christian Chavez <christian.chavez@moller.no>
2017-03-22 10:15:53 +0100

    Meh!

--- taskchecker.py
+++ taskchecker.py
@@ -2,10 +2,10 @@ from subprocess import Popen, PIPE, call
 import re
 
 pipe = Popen(["git", "log", "-1"], stdout=PIPE)
-gitLog = pipe.communicate()[0]
+stdout, stderr = pipe.communicate()
 
-matchObj = re.search(r"Task\s+(\d+)",gitLog, flags=0)
+matchObj = re.search(r"Task\s+(\d+)", stdout, flags=0)
 
 if not matchObj:
     call("echo 'Your commit message needs to contain Task XXXX where X is number.'",shell=1)
-    call(["exit","1"], shell=1) 
+    call(["exit","1"], shell=1)
